be8fa74a600b86c69b6be3286cf92f0b7898dd29
Bez1887 <behzadpaki@gmail.com>
2023-05-22 15:01:10 +0200

    fertig

---
+++ modules/index.js
@@ -0,0 +1,9 @@
+////////// Import
+
+import { clickImages } from "./click.js";
+import { hoverHeader } from "./hover.js";
+import { scrollWindow } from "./scroll.js";
+
+scrollWindow();
+hoverHeader();
+clickImages();
